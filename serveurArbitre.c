080d118bd56e020e1cdd2572f4b8f42e4f2c6519
Axel Ledermann <33054741+Valariael@users.noreply.github.com>
2020-03-08 01:50:09 +0100

    server side game coms structure + :recycle:

--- serveurArbitre.c
+++ serveurArbitre.c
@@ -5,69 +5,86 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/select.h>
 #include <string.h>
 #include "libSockets.h"
 #include "protocolQuantik.h"
 #include "validation.h"
 
-int paireJoueurValides (int *sockJoueur1, int *sockJoueur2, int sockConnexion)
+void connecteJoueur(int *sockJoueur, int sockConnexion, TPartieReq *reqPartie)
 {
 	int tailleAdr, err;
-	struct sockaddr_in adrJoueur1, adrJoueur2;
+	struct sockaddr_in adrJoueur;
+
+	//Tant que la connexion d'un joueur n'a pas réussi on réessaie.
+	while (*sockJoueur <= 0)
+	{
+		*sockJoueur = accept(sockConnexion,
+			(struct sockaddr *)&adrJoueur,
+			(socklen_t *)&tailleAdr);
+		if (*sockJoueur < 0) 
+		{
+			perror("arbitre> erreur connexion joueur");
+			continue;
+		}
+		printf("arbitre> joueur connecté fd=%d\n", *sockJoueur);
+
+		err = recv(*sockJoueur, reqPartie, sizeof(TPartieReq), 0);
+		if (err <= 0)
+		{//TODO: check idReq is PARTIE not COUP > ERR_TYP
+			perror("arbitre> erreur recv partie");
+			shutdown(*sockJoueur, SHUT_RDWR);
+			close(*sockJoueur);
+			*sockJoueur = 0;
+			continue;
+		}
+		printf("arbitre> nom joueur=%s\n", reqPartie->nomJoueur);
+	}
+}
+
+int ackJoueursConnectes(int *sockJoueur, int *sockAutreJoueur, TPartieRep *repPartie, TPartieRep *repPartieAutre)
+{
+	int err;
+
+	//Envoi de la réponse à la requête de partie.
+	err = send(*sockJoueur, repPartie, sizeof(TPartieRep), 0);
+	if (err <= 0)
+	{
+		perror("arbitre> erreur send partie");
+		shutdown(*sockJoueur, SHUT_RDWR);
+		close(*sockJoueur);
+		*sockJoueur = 0;
+
+		repPartieAutre->err = ERR_PARTIE;
+		err = send(*sockAutreJoueur, repPartieAutre, sizeof(TPartieRep), 0);
+		if (err <= 0) 
+		{
+			perror("arbitre> erreur send erreur partie");
+			shutdown(*sockAutreJoueur, SHUT_RDWR);
+			close(*sockAutreJoueur);
+			*sockAutreJoueur = 0;
+			return -2;
+		}
+		//TODO close ?
+
+		return -1;
+	}
+}
+
+int paireJoueurValides (int *sockJoueur1, int *sockJoueur2, int sockConnexion)
+{
+	int err;
 	TPartieReq reqPartie1, reqPartie2;
 	TPartieRep repPartie1, repPartie2;
 
 	*sockJoueur1 = 0;
 	*sockJoueur2 = 0;
-	//Connexion premier joueur.
-	while (*sockJoueur1 <= 0)
-	{
-		*sockJoueur1 = accept(sockConnexion,
-			(struct sockaddr *)&adrJoueur1,
-			(socklen_t *)&tailleAdr);
-		if (*sockJoueur1 < 0) 
-		{
-			perror("arbitre> erreur accept joueur 1");
-			continue;
-		}
-		printf("arbitre> j1 fd=%d\n", *sockJoueur1);
 
-		err = recv(*sockJoueur1, &reqPartie1, sizeof(TPartieReq), 0);
-		if (err <= 0)
-		{//TODO: check idReq is PARTIE not COUP > ERR_TYP
-			perror("arbitre> erreur recv partie 1");
-			shutdown(*sockJoueur1, SHUT_RDWR);
-			close(*sockJoueur1);
-			*sockJoueur1 = 0;
-			continue;
-		}
-		printf("arbitre> j1 nom=%s\n", reqPartie1.nomJoueur);
-	}
+	//Connexion premier joueur.
+	connecteJoueur(sockJoueur1, sockConnexion, &reqPartie1);
 
 	//Connexion deuxième joueur.
-	while (*sockJoueur2 <= 0) 
-	{
-		*sockJoueur2 = accept(sockConnexion,
-			(struct sockaddr *)&adrJoueur2,
-			(socklen_t *)&tailleAdr);
-		if (*sockJoueur2 < 0) 
-		{
-			perror("arbitre> erreur accept joueur 2");
-			continue;
-		}
-		printf("arbitre> j2 fd=%d\n", *sockJoueur2);
-
-		err = recv(*sockJoueur2, &reqPartie2, sizeof(TPartieReq), 0);
-		if (err <= 0)
-		{//TODO: check idReq is PARTIE not COUP > ERR_TYP
-			perror("arbitre> erreur recv partie 2");
-			shutdown(*sockJoueur2, SHUT_RDWR);
-			close(*sockJoueur2);
-			*sockJoueur2 = 0;
-			continue;
-		}
-		printf("arbitre> j2 nom=%s\n", reqPartie2.nomJoueur);
-	}
+	connecteJoueur(sockJoueur2, sockConnexion, &reqPartie2);
 
 	//Préparation des réponses.
 	repPartie1.err = ERR_OK;
@@ -85,54 +102,126 @@ int paireJoueurValides (int *sockJoueur1, int *sockJoueur2, int sockConnexion)
 	}
 
 	//Envoi réponse au premier joueur.
-	err = send(*sockJoueur1, &repPartie1, sizeof(TPartieRep), 0);
-	if (err <= 0)
-	{
-		perror("arbitre> erreur send partie 1");
-		shutdown(*sockJoueur1, SHUT_RDWR);
-		close(*sockJoueur1);
-		*sockJoueur1 = 0;
-
-		repPartie2.err = ERR_PARTIE;
-		err = send(*sockJoueur2, &repPartie2, sizeof(TPartieRep), 0);
-		if (err <= 0) 
-		{
-			perror("arbitre> erreur send erreur partie 2");
-			shutdown(*sockJoueur2, SHUT_RDWR);
-			close(*sockJoueur2);
-			*sockJoueur2 = 0;
-			return -2;
-		}
-		//TODO close ?
-
-		return -1;
-	}
+	err = ackJoueursConnectes(sockJoueur1, sockJoueur2, &repPartie1, &repPartie2);
 
 	//Envoi réponse au deuxième joueur.
-	err = send(*sockJoueur2, &repPartie2, sizeof(TPartieRep), 0);
-	if (err <= 0)
-	{
-		perror("arbitre> erreur send partie 2");
-		shutdown(*sockJoueur2, SHUT_RDWR);
-		close(*sockJoueur2);
-		*sockJoueur2 = 0;
+	err = ackJoueursConnectes(sockJoueur2, sockJoueur1, &repPartie2, &repPartie1);
 
-		repPartie1.err = ERR_PARTIE;
-		err = send(*sockJoueur1, &repPartie1, sizeof(TPartieRep), 0);
-		if (err <= 0) 
+	return 0;
+}
+
+int traiterCoup(int sockJoueur, int sockAutreJoueur, int sockJoueurCourant, int numJoueur)
+{
+	if (sockJoueurCourant == sockJoueur)
+	{
+		err = recv(sockJoueur, &reqCoup, sizeof(TCoupReq), 0);
+		if (err <= 0)
 		{
-			perror("arbitre> erreur send erreur partie 1");
-			shutdown(*sockJoueur1, SHUT_RDWR);
-			close(*sockJoueur1);
-			*sockJoueur1 = 0;
-			return -4;
+			perror("arbitre> erreur recv coup 1");
+			shutdown(sockJoueur, SHUT_RDWR);
+			shutdown(sockAutreJoueur, SHUT_RDWR);
+			close(sockJoueur);
+			close(sockAutreJoueur);
 		}
-		//TODO close ?
-		
-		return -3;
+		else
+		{
+			coupValide = validationCoup(numJoueur, reqCoup, &resultatValidation);
+			if (!coupValide || (reqCoup.propCoup != resultatValidation))
+			{
+				//TODO : triche
+			}
+			else
+			{
+
+			}
+		}
+	}
+	else 
+	{
+		//TODO: pas son tour
 	}
 
 	return 0;
+}
+
+int jouerPartie(int sockJoueur1, int sockJoueur2)
+{
+	TCoupReq reqCoup;
+	TCoupRep repCoup;
+	int err, nsfd, sockJoueurCourant = sockJoueur1, continuerPartie;
+	struct timeval timeout;
+	fd_set readSet;
+	TPropCoup resultatValidation;
+	bool coupValide;
+	
+	initialiserPartie();
+	while (continuerPartie)  //TODO: not infinite
+	{
+
+		FD_ZERO(&readSet);
+		FD_SET(sockJoueur1, &readSet);
+		FD_SET(sockJoueur2, &readSet);
+		timeout.tv_sec = 5;
+		nsfd = (sockJoueur1 > sockJoueur2 ? sockJoueur1 + 1 : sockJoueur2 + 1);
+
+		err = select(nsfd, &readSet, NULL, , NULL, &timeout);
+		if (err < 0) 
+		{
+			perror("arbitre> erreur select");
+			//TODO: interruption partie ???
+			shutdown(sockJoueur1, SHUT_RDWR);
+			shutdown(sockJoueur2, SHUT_RDWR);
+			close(sockJoueur1);
+			close(sockJoueur2);
+			return -1;
+		} 
+		else if (err == 0)
+		{
+			printf("arbitre> fd=%d temps écoulé\n", sockJoueurCourant);
+			//TODO: timeout
+		}
+		else
+		{
+			if (FD_ISSET(sockJoueur1, &readSet) != 0)
+			{
+				err = traiterCoup(sockJoueur1, sockJoueur2, sockJoueurCourant, 2);
+				if (err < 0)
+				{
+					perror("arbitre> erreur traitement coup 1");
+					//TODO: interruption partie ???
+					shutdown(sockJoueur1, SHUT_RDWR);
+					shutdown(sockJoueur2, SHUT_RDWR);
+					close(sockJoueur1);
+					close(sockJoueur2);
+					return -2;
+				}
+				else if (err > 0)
+				{
+					continuerPartie = 0;
+					continue; //TODO: a changer
+				}
+			}
+			if (FD_ISSET(sockJoueur2, &readSet) != 0)
+			{
+				err = traiterCoup(sockJoueur2, sockJoueur1, sockJoueurCourant, 2);
+				if (err < 0)
+				{
+					perror("arbitre> erreur traitement coup 2");
+					//TODO: interruption partie ???
+					shutdown(sockJoueur1, SHUT_RDWR);
+					shutdown(sockJoueur2, SHUT_RDWR);
+					close(sockJoueur1);
+					close(sockJoueur2);
+					return -3;
+				}
+				else if (err > 0)
+				{
+					continuerPartie = 0;
+					continue; //TODO: a changer
+				}
+			}
+		}
+	}
 }
 
 int main(int argc, char **argv)
@@ -187,7 +276,6 @@ int main(int argc, char **argv)
 
 			case -1:
 			perror("arbitre> erreur fork jeu");
-			shutdown(sockConnexion, SHUT_RDWR);
 			close(sockConnexion);
 			shutdown(sockJoueur1, SHUT_RDWR);
 			close(sockJoueur1);
@@ -204,7 +292,7 @@ int main(int argc, char **argv)
 	}
 
 	printf("arbitre> début jeu\n");
-	//jeu
-
+	//Première manche
+	// jouerPartie(joueurBlancSock, jnoir etc)
 	return 0;
 }
