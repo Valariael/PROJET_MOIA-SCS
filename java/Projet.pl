a6bc3d43b8276047c2f4f30a25194a314479b65d
Ashtaga <mediplouf@hotmail.fr>
2020-03-12 15:49:59 +0100

    started prolog AI :construction:

---
+++ java/Projet.pl
@@ -0,0 +1,48 @@
+%listes de paires forme de pion - couleur, chaque case du tableau représente un case du jeu à un instant T
+plateau(0,[]).
+plateau(N, [[0,0]|L]):-
+    E is N-1,
+    plateau(E,L).
+
+
+%liste des indices disponibles du tableau
+listeIndice(17,[]).
+listeIndice(N, [N|L]):-
+    E is N+1,
+    listeIndice(E,L).
+
+
+%prédicats pour initialiser les joueurs
+%le premier chiffre représente le joueur
+%les paires représentent le nombre de pion restant au joueur pour chaque type
+joueur1([1,[[2,1],[2,2],[2,3],[2,4]]]).
+joueur2([2,[[2,1],[2,2],[2,3],[2,4]]]).
+
+%TODO grille des indices, faire un select sur la première case dispo, envoyer l'indice dans placer
+%place une forme sur une position
+placer(Grille,NouvelleGrille,Pos,Duo):-nth1(Pos,NouvelleGrille,Duo,Grille). 
+
+
+%vérifie que la forme est sur le plateau
+placerCorrect(Pos):-Pos>0, Pos < 17.
+
+
+associationCarre(A,B):-select(A,[1,2,5,6],B).
+associationCarre(A,B):-select(A,[3,4,7,8],B).
+associationCarre(A,B):-select(A,[9,10,13,14],B).
+associationLigne(A,B):-select(A,[1,2,3,4],B).
+associationLigne(A,B):-select(A,[5,6,7,8],B).
+associationLigne(A,B):-select(A,[9,10,11,12],B).
+associationLigne(A,B):-select(A,[13,14,15,16],B).
+associationColonne(A,B):-select(A,[1,5,9,13],B).
+associationColonne(A,B):-select(A,[2,6,10,14],B).
+associationColonne(A,B):-select(A,[3,7,11,15],B).
+associationColonne(A,B):-select(A,[4,8,12,16],B).
+%vérifie les contraintes imposées 
+placerContrainteLigne(Grille, Joueur, [Nombre,Forme],Pos):-
+    Nombre>0,
+    contrainteLigne(Grille)
+
+%etatFinal() vérifie l'état final
+%longueur
+%Sol
