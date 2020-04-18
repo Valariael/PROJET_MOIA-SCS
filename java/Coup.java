8c6c37179a56f08c5ca87f51f58495caae445e09
Axel Ledermann <33054741+Valariael@users.noreply.github.com>
2020-04-18 03:12:44 +0200

    javadoc Coup :books:

--- java/Coup.java
+++ java/Coup.java
@@ -1,3 +1,6 @@
+/**
+ * Une instance de Coup représente un mouvement du jeu Quantik avec ses différentes caractéristiques.
+ */
 public class Coup
 {
     // Un Coup est représenté sous la même forme que la structure en C du côté client.
@@ -10,16 +13,28 @@ public class Coup
         return bloque == 0;
     }
 
+    /**
+     * Getter pour récupérer l'indice à destination de Prolog à partir de la ligne et de la colonne.
+     * @return un entier représentant l'indice du coup
+     */
     public int getIndicePl()
     {
         return 4 * ligne + colonne +1;
     }
 
+    /**
+     * Getter pour récupérer le pion à destination de prolog, les numéros étant décalés.
+     * @return un entier représentant le pion du coup
+     */
     public int getPionPl()
     {
         return pion + 1;
     }
 
+    /**
+     * Setter pour stocker un indice venant de Prolog sous la forme d'un numéro de ligne et de colonne.
+     * @param indice un entier représentant l'indice du coup
+     */
     public void setLigneColonnePl(int indice)
     {
         indice--;
@@ -27,6 +42,10 @@ public class Coup
         colonne = indice % 4;
     }
 
+    /**
+     * Setter pour stocker le pion venant de prolog, les numéros étant décalés.
+     * @return un entier représentant le pion du coup
+     */
     public void setPionPl(int pion)
     {
         this.pion = pion - 1;
