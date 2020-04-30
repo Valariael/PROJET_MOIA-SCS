e9f4ef00663c02d1c68f24234a69ad89345c1841
Ashtaga <mediplouf@hotmail.fr>
2020-04-30 17:44:01 +0200

    AJout joueur.sh

---
+++ joueur.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#sh joeur.sh hostServeur portServeur nomJoueur [*]
+
+if [ $# -ne 3 ]; then
+    if [ $# -ne 4 ]; then
+    echo "Usage : ./soloPlay.sh <hostServeur> <portServeur> <nomJoueur>[<portIA>]"
+    exit -1
+    else 
+        portIA=true
+    fi
+else portIA=false
+fi
+clear
+cd java
+if [[ $LD_PRELOAD != *"libswipl.so"* ]]; then
+	export LD_PRELOAD=libswipl.so:${LD_PRELOAD}
+fi
+if [[ $LD_LIBRARY_PATH != *"/usr/lib/swi-prolog/lib/amd64/"* ]]; then
+	export LD_LIBRARY_PATH=/usr/lib/swi-prolog/lib/amd64/:${LD_LIBRARY_PATH}
+fi
+if [[ $CLASSPATH != *"/usr/lib/swi-prolog/lib/jpl.jar"* ]]; then
+	export CLASSPATH=/usr/lib/swi-prolog/lib/jpl.jar:${CLASSPATH}
+fi
+make
+err=$?
+if [ $err -ne "0" ]; then
+	echo "Erreur à la compilation du moteur IA !"
+	exit -1
+fi
+if [ "$portIA" = false ]; then
+java MoteurIA 6666 1 &
+echo "Moteur IA de $3 lancé."
+else
+java MoteurIA $4 1 &
+echo "Moteur IA de $3 lancé."
+fi
+cd ..
+make
+err=$?
+if [ $err -ne "0" ]; then
+	echo "Erreur à la compilation du serveur/client !"
+	exit -1
+fi
+if [ "$portIA" = false ]; then
+./clientJoueur $1 $2 $3 N 6666 &
+echo "Client blanc lancé."
+else 
+./clientJoueur $1 $2 $3 N $4 &
+echo "Client blanc lancé."
+fi
